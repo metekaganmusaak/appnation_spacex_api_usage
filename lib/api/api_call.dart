0ba25ee7ceb7947f1f6f8bf07d9730771e0df629
metekaganmusaak <metemusaak@gmail.com>
2021-08-05 15:28:38 +0300

    I fixed minor mistakes at ApiCall

--- lib/api/api_call.dart
+++ lib/api/api_call.dart
@@ -4,19 +4,14 @@ import 'package:appnation_spacex/model/spacex_model.dart';
 import 'package:http/http.dart' as http;
 
 class ApiCall {
-  
   static Future<SpaceX> apiCall() async {
-    final response = await http.get(Uri.parse('https://api.spacexdata.com/v4/launches/latest'));
+    final response = await http
+        .get(Uri.parse('https://api.spacexdata.com/v4/launches/latest'));
 
-    try{
-      // ignore: unrelated_type_equality_checks
-      if(response == 200)
-    {
+    if (response.statusCode == 200) {
       return SpaceX.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Something went wrong');
     }
-    }catch(e){
-      throw Exception(e);
-    }
-    return SpaceX.fromJson(json.decode(response.body));
   }
 }
